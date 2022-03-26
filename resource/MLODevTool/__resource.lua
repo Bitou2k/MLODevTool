9da701250ba9b87ced1acc9848fb82b75176e107
FRANK <89828815+FRANK-Infinity@users.noreply.github.com>
2022-03-26 15:06:41 +0530

    Delete __resource.lua
    Delete __resource.lua And Add fxmanifest.lua

--- resource/MLODevTool/__resource.lua
+++
@@ -1,7 +0,0 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
-resource_version '1.0'
-
-name 'B2K MLO Dev Tool'
-description 'B2KRP MLO Dev Tool'
-
-client_script 'MLODevTool.net.dll'
